40199da0b8f18f86998ced40e2cbd70c44ff921a
JansenBhodi <117980774+JansenBhodi@users.noreply.github.com>
2025-10-14 13:35:14 +0200

    unit test getbyname
    first working unit test written using moq. yippieee

---
+++ BackEnd_Testing/Unit Testing/MmoPlayerTesting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Moq;
+using Moq.Protected;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Logic;
+using BusinessLogic.DbCalls;
+using BusinessLogic.Classes;
+
+namespace BackEnd_Testing.Unit_Testing
+{
+    [TestClass]
+    public class MmoPlayerTesting
+    {
+        //We follow the AAA method in unit testing here.
+        #region Get Player by name tests
+        //unit tests for the functionality surrounding getting a player by their unique name.
+        [TestMethod]
+        public void SuccessfullyRetrievePlayer()
+        {
+            //Arrange
+            var playerDbMock = new Mock<ImmoPlayerCalls>();
+            string name = "Charlie";
+            playerDbMock.Setup(p => p.GetPlayerByName(name)).Returns(
+                                new MmoPlayer(4, name, (Roleclass)5));
+            PlayersLogic logic = new PlayersLogic(playerDbMock.Object);
+
+            //Act
+            MmoPlayer result = logic.GetPlayer(name);
+
+            //Assert
+            playerDbMock.Verify(p => p.GetPlayerByName(name), Times.Once);
+            Assert.AreEqual(name, result.Name);
+        }
+        #endregion
+    }
+}
